3d0d1d1f8d78073dd19f349be15e6da3dbef05bc
Desyatkov Evgeny <zdesyatkov1998@gmail.com>
2023-12-11 23:09:08 +0300

    (add 3 step)

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,1 +1,5 @@
 console.log ("Welcome to the Brain Games!");
+import readlineSync from 'readline-sync';
+
+const name = readlineSync.question('May I have your name?');
+console.log ('Hello,', name, '!');
