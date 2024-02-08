f0ff7a9f602de8cb001d318bb880a5d5e91fe9c8
Desyatkov Evgeny <zdesyatkov1998@gmail.com>
2024-02-08 22:06:01 +0300

    fix brain-prime

--- src/prime.js
+++ src/prime.js
@@ -8,8 +8,8 @@ const userName = getName();
  
 
 // Определение функции, дающее рандомное целое число
-const getRandomInt = (max) => {
-  return Math.floor(Math.random() * max);
+const getRandomInt = (min, max) => {
+  return Math.floor(Math.random(max - min) * max);
 };
  
 // Определение функции игры на четность
@@ -18,24 +18,29 @@ export const primeGame = () => {
   console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
  
   for (let i = 0; i < 3; i += 1) {
-    const number = getRandomInt(10);
+    const number = getRandomInt(1, 10);
     let sum = 0;
     console.log('Question: ', number);
     const answerUser = readlineSync.question('Your answer: ');
- 
-    for(let j = 1; j < Number(answerUser); j +=1) {
-        if (Number(answerUser) % j === 0) {
+
+    for(let j = 1; j <= number; j +=1) {
+        if (number % j === 0) {
             sum += 1;
         }
-
-    }
-
-    if (sum <= 2) {
+      }
+    const checking1 = (sum === 2) && (answerUser === 'yes');
+    const checking2 = (sum !== 2) && (answerUser === 'no');
+    
+      console.log(sum);
+    if (checking1 === true || checking2 === true) {
       console.log('Correct!');
     } else {
       const opposite = (answerUser === 'yes' ? 'no' : 'yes');
-      const errorMessage = `"${answerUser}" is wrong answer ;(. /n Let's try again, ${userName}!`;
-      return errorMessage;
+      const errorMessage1 = (`'${answerUser}' is wrong answer ;(. Correct answer was '${opposite}'.`);
+      const errorMessage2 = (`Let's try again, ${userName}!`);
+      console.log(errorMessage1);
+      console.log(errorMessage2);
+      return;
     }
   }
  
