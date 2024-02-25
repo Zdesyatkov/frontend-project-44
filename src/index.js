206ae8b2df2867902d14336a9a10015bd06fac06
Desyatkov Evgeny <zdesyatkov1998@gmail.com>
2024-02-25 22:47:46 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const basisOfGames = (noteToGame, task) => {
   // Начало цикла игры
   for (let i = 0; i < 3; i += 1) {
     const pairOfQuestionAndResult = task();
-    console.log('Question: ', pairOfQuestionAndResult[0]);
+    console.log('Question:', pairOfQuestionAndResult[0]);
     const answerUser = readlineSync.question('Your answer: ');
 
     // Константа для правильного ответа
