6432cce386b758c5607d16a10ee2eb1a92fb6390
Desyatkov Evgeny <zdesyatkov1998@gmail.com>
2024-02-26 22:18:26 +0300

    all OK

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,20 +4,20 @@ import basisOfGames from '../index.js';
 const getRandomInt = (min, max) => Math.floor(Math.random(max - min) * max);
 // Определение функции игры прогрессия
 
-  const noteToProgression = 'What number is missing in the progression?';
-  const taskProgression = () => {
-    let number1 = getRandomInt(1, 10);
-    const number2 = getRandomInt(1, 10);
-    const randomNumber = getRandomInt(1, 8);
-    const arr = [];
-    for (let j = 0; j < 8; j += 1, (number1 += number2)) {
-      arr[j] = number1;
-    }
-    const removed = arr.splice(randomNumber, 1, '..');
-    const question = arr.join(' ');
-    const result = +removed.join('');
-    return [question, result.toString()];
-  };
-  export default () => {
-    basisOfGames(noteToProgression, taskProgression);
+const noteToProgression = 'What number is missing in the progression?';
+const taskProgression = () => {
+  let number1 = getRandomInt(1, 10);
+  const number2 = getRandomInt(1, 10);
+  const randomNumber = getRandomInt(1, 8);
+  const arr = [];
+  for (let j = 0; j < 8; j += 1, (number1 += number2)) {
+    arr[j] = number1;
   }
+  const removed = arr.splice(randomNumber, 1, '..');
+  const question = arr.join(' ');
+  const result = +removed.join('');
+  return [question, result.toString()];
+};
+export default () => {
+  basisOfGames(noteToProgression, taskProgression);
+};
