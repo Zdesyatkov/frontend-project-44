133ad6c3a93e004f3a4ce3c7b72f9ebe29d1f66a
Desyatkov Evgeny <zdesyatkov1998@gmail.com>
2024-02-26 20:09:10 +0300

    fix calcGame

--- src/games/calc.js
+++ src/games/calc.js
@@ -2,34 +2,35 @@ import basisOfGames from '../index.js';
 
 // Определение функции игры калькулятор
 
-export default function calcGame() {
-  const noteToCalc = 'What is the result of the expression?';
-  const getRandomInt = (max) => Math.floor(Math.random() * max);
-  const taskCalc = () => {
-    const number1 = getRandomInt(10);
-    const number2 = getRandomInt(10);
+const noteToCalc = 'What is the result of the expression?';
+const getRandomInt = (max) => Math.floor(Math.random() * max);
+const taskCalc = () => {
+  const number1 = getRandomInt(10);
+  const number2 = getRandomInt(10);
 
-    const signs = ['+', '-', '*'];
-    const sign = signs[getRandomInt(3)];
+  const signs = ['+', '-', '*'];
+  const sign = signs[getRandomInt(3)];
 
-    const question = `${number1} ${sign} ${number2}`;
+  const question = `${number1} ${sign} ${number2}`;
 
-    let result = 0;
+  let result = 0;
 
-    switch (sign) {
-      case '-':
-        result = number1 - number2;
-        break;
-      case '+':
-        result = number1 + number2;
-        break;
-      case '*':
-        result = number1 * number2;
-        break;
-      default:
-        result = null;
-    }
-    return [question, result];
-  };
+  switch (sign) {
+    case '-':
+      result = number1 - number2;
+      break;
+    case '+':
+      result = number1 + number2;
+      break;
+    case '*':
+      result = number1 * number2;
+      break;
+    default:
+      result = null;
+  }
+  return [question, result];
+};
+
+export default () => {
   basisOfGames(noteToCalc, taskCalc);
-}
+};
